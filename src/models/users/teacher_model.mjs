8d00509ad7d6e94326a647bafb8f904d07f04eca
jnthnexpdev <jnthnexpdev@gmail.com>
2024-12-10 18:29:02 -0600

    Actualizacion de modelos

--- src/models/users/teacher_model.mjs
+++ src/models/users/teacher_model.mjs
@@ -1,5 +1,5 @@
-import bcrypt from bcrypt;
-import mongoose from mongoose;
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
 const schema = mongoose.Schema;
 
 const teacherModel = new schema({
