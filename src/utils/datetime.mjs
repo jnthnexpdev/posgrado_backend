f25cf3250b9bb18f177278176d13ac6fde834596
jnthnexpdev <jnthnexpdev@gmail.com>
2025-01-27 12:56:41 -0600

    Api de tiempo de respaldo

--- src/utils/datetime.mjs
+++ src/utils/datetime.mjs
@@ -2,16 +2,28 @@ import axios from 'axios';
 import moment from 'moment';
 
 export const getDateTime = async () => {
-    try{
-        const response = await axios.get('http://worldtimeapi.org/api/timezone/America/Mexico_City');
-        const { date } = response.data;
-
-        const formatDate = moment(date).format('DD-MM-YYYY');
-        const formatTime = moment(date).format('h:mm A');
-
-        return { fecha : formatDate, hora : formatTime };
-    }catch(error){
-        console.error('Ha ocurrido un error al obtener la hora y fecha');
-        throw error;
+  try {
+    // Intenta obtener los datos de la primera API
+    const response = await axios.get('http://worldtimeapi.org/api/timezone/America/Mexico_City');
+    return formatDateTime(response.data.datetime);
+  } catch (error) {
+    console.warn('Primera API falló, intentando con la API de respaldo...');
+    
+    try {
+      // Si falla la primera API, intenta con la segunda
+      const backupResponse = await axios.get('https://timeapi.io/api/Time/current/zone?timeZone=America/Mexico_City');
+      console.log(formatDateTime(backupResponse.data.dateTime));
+      return formatDateTime(backupResponse.data.dateTime);
+    } catch (backupError) {
+      console.error('Ambas APIs fallaron al obtener la hora y fecha.');
+      throw backupError;
     }
-}
+  }
+};
+
+// Función para formatear la fecha y hora
+const formatDateTime = (datetime) => {
+  const formatDate = moment(datetime).format('DD-MM-YYYY');
+  const formatTime = moment(datetime).format('h:mm A');
+  return { fecha: formatDate, hora: formatTime };
+};
